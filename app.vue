4d99ec354c570255e9dc4240b5a338716940976a
AmauriBlanco <blancoamauri@gmail.com>
2024-11-27 12:31:22 -0300

    2-4 Link entre páginas

--- app.vue
+++ app.vue
@@ -1,5 +1,17 @@
 <template>
+    <nav>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="carros">Carros</NuxtLink>
+        <NuxtLink to="motos">Motos</NuxtLink>
+    </nav>
     <div>
         <NuxtPage />
     </div>
 </template>
+
+<style>
+nav {
+    display: flex;
+    gap: 1rem;
+}
+</style>
---
+++ pages/motos/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Essa é a nova página de Motos</h1>
+    </div>
+</template>
