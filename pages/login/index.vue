fe451a01e863c05e9903d7278dc4d45aab244d7d
AmauriBlanco <blancoamauri@gmail.com>
2024-11-28 10:42:52 -0300

    3-2 Layout personalizado

---
+++ pages/login/index.vue
@@ -0,0 +1,22 @@
+<script setup>
+definePageMeta({
+    layout: "login",
+});
+</script>
+
+<template>
+    <div>
+        <h1>Faça seu Login</h1>
+        <form>
+            <div>
+                <label for="email">E-mail:</label>
+                <input type="email" name="" id="email" />
+            </div>
+            <div>
+                <label for="password">Senha:</label>
+                <input type="password" name="" id="password" />
+            </div>
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
+</template>
