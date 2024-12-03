1ea038ddb13cadb5c43b9b6907b3a37de39168ae
AmauriBlanco <blancoamauri@gmail.com>
2024-12-03 18:35:43 -0300

    5-5 Entendendo os getters

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,17 @@
 <script setup>
 const counter = useCounterStore();
+const mensagem = ref("");
+
+const atualizarMensagem = () => {
+    counter.updateMessage(mensagem.value);
+    mensagem.value = "";
+};
 </script>
 
 <template>
     <TitlePage text="Index" />
     <Card>
-        <ul class="bg-red-500 mt-20">
+        <ul class="bg-red-500 mt-8">
             <li><Icon name="logos:nuxt-icon" /> item 1</li>
             <li><Icon name="logos:vue" /> item 2</li>
         </ul>
@@ -13,8 +19,14 @@ const counter = useCounterStore();
 
     <div class="py-4">
         <p>Contagem atual: {{ counter.count }}</p>
+        <p>Contagem dobrada: {{ counter.doubleCount }}</p>
 
-       
+        <button
+            class="bg-blue-500 text-white rounded-lg py-1 px-2"
+            @click="counter.increment"
+        >
+            Adicionar 1
+        </button>
         <button
             class="bg-red-500 text-white rounded-lg py-1 px-2 ms-2"
             @click="counter.decrement"
@@ -22,6 +34,18 @@ const counter = useCounterStore();
             Remover 1
         </button>
     </div>
+    <div class="py-4">
+        <p>A mensagem é: {{ counter.message }}</p>
+        <p>A mensagem tem {{ counter.messageLength }} caracteres</p>
+        <input v-model="mensagem" class="border border-black" />
+        <button
+            @click="atualizarMensagem"
+            class="bg-blue-500 text-white rounded-lg py-1 px-2 mt-2"
+        >
+            Atualizar Mensagem
+        </button>
+    </div>
+    <div class="mb-80"></div>
 </template>
 
 <style>
--- stores/contador.ts
+++ stores/contador.ts
@@ -3,13 +3,22 @@ import { defineStore } from "pinia";
 export const useCounterStore = defineStore("counter", {
     state: () => ({
         count: 0,
+        message: "TreinaWeb",
     }),
     actions: {
         increment() {
             this.count++;
         },
         decrement() {
-          if(this.count > 0) this.count--;
+            if (this.count > 0) this.count--;
         },
+        updateMessage(newMessage: string) {
+            this.message = newMessage;
+        },
+    },
+
+    getters: {
+        doubleCount: (state) => state.count * 2,
+        messageLength: (state) => state.message.length,
     },
 });
